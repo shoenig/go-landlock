513ae700eb57b99178e1bf8051177f9128d05e06
Seth Hoenig <shoenig@duck.com>
2022-11-04 22:45:57 +0000

    tests: some write tests

--- landlock_linux_test.go
+++ landlock_linux_test.go
@@ -5,14 +5,20 @@ package landlock
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/shoenig/test/must"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 func TestLocker_New(t *testing.T) {
 	t.Run("empty", func(t *testing.T) {
@@ -265,6 +271,20 @@ func TestLocker_writes(t *testing.T) {
 			_, err = os.ReadFile(f)
 			must.NoError(t, err) // has permission
 		},
+		"dir_w": func() {
+			f := tmpFile(t, "hello.txt", "hi")
+			l := New(Dir(filepath.Dir(f), "w"))
+			err := l.Lock(Enforce)
+			must.NoError(t, err)
+			w, err := os.OpenFile(f, os.O_WRONLY, 0o644)
+			must.NoError(t, err)
+			_, err = io.WriteString(w, "test")
+			must.NoError(t, err)
+			err = w.Close()
+			must.NoError(t, err)
+			_, err = os.ReadFile(f)
+			must.Error(t, err) // no permission
+		},
 	}
 
 	// This part gets run in each sub-process; it is the actual test
@@ -280,6 +300,67 @@ func TestLocker_writes(t *testing.T) {
 	// just once.
 	for name := range cases {
 		arg := fmt.Sprintf("-test.run=TestLocker_writes/%s", name)
+		cmd := exec.Command(os.Args[0], arg)
+		cmd.Env = append(os.Environ(), fmt.Sprintf("TEST=%s", name))
+		b, err := cmd.CombinedOutput()
+		t.Logf("TEST[%s] (arg: %s)\n\t|> %s\n\n", name, arg, string(b))
+		must.NoError(t, err)
+	}
+}
+
+// random will create a random text file name
+func random() string {
+	n := 6
+	b := make([]byte, n)
+	for i := 0; i < n; i++ {
+		b[i] = byte(rand.Int()%26 + 97)
+	}
+	return string(b) + ".txt"
+}
+
+func TestLocker_creates(t *testing.T) {
+	cases := map[string]func(){
+		"none": func() {
+			l := New() // none
+			err := l.Lock(Enforce)
+			must.NoError(t, err)
+			_, err = os.OpenFile("/tmp/a.txt", os.O_CREATE, 0o0644)
+			must.Error(t, err)
+		},
+		"dir_rw": func() {
+			f := filepath.Join(os.TempDir(), random())
+			dir := filepath.Dir(f)
+			l := New(Dir(dir, "rw"))
+			err := l.Lock(Enforce)
+			must.NoError(t, err)
+			_, err = os.OpenFile(f, os.O_CREATE, 0o644)
+			must.Error(t, err) // rw only, not create
+		},
+		"dir_rwc": func() {
+			f := filepath.Join(os.TempDir(), random())
+			dir := filepath.Dir(f)
+			l := New(Dir(dir, "rwc"))
+			err := l.Lock(Enforce)
+			must.NoError(t, err)
+			_, err = os.OpenFile(f, os.O_CREATE, 0o644)
+			must.NoError(t, err)
+		},
+		"file_rwc": func() {
+			f := filepath.Join(os.TempDir(), random())
+			l := New(File(f, "rwc"))
+			err := l.Lock(Enforce)
+			must.Error(t, err) // no such file
+		},
+	}
+
+	if name := os.Getenv("TEST"); name != "" {
+		f := cases[name]
+		f()
+		return
+	}
+
+	for name := range cases {
+		arg := fmt.Sprintf("-test.run=TestLocker_creates/%s", name)
 		cmd := exec.Command(os.Args[0], arg)
 		cmd.Env = append(os.Environ(), fmt.Sprintf("TEST=%s", name))
 		b, err := cmd.CombinedOutput()
