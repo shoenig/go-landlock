950264068907f2cba0593092a843407ed85c416c
Seth Hoenig <shoenig@duck.com>
2022-11-05 09:50:08 -0500

    paths: support for pre-made paths for common modes (#7)

--- path.go
+++ path.go
@@ -3,6 +3,7 @@ package landlock
 import (
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -45,24 +46,85 @@ func (p *Path) access() rule {
 
 var shared []*Path
 
-// todo: stat & load
-func init() {
-	shared = []*Path{
-		// devices
-		File("/dev/null", "rw"),
+var stdio []*Path
 
-		// libs
+var tmp []*Path
+
+var tty []*Path
+
+var vminfo []*Path
+
+var dns []*Path
+
+func init() {
+	shared = load([]*Path{
+		File("/dev/null", "rw"),
 		Dir("/lib", "rx"),
 		Dir("/lib64", "rx"),
 		Dir("/usr/lib", "rx"),
 		Dir("/usr/local/lib", "rx"),
-		// Dir("/usr/local/lib64", "rx"),
-
-		// linker
+		Dir("/usr/local/lib64", "rx"),
 		File("/etc/ld.so.conf", "r"),
 		File("/etc/ld.so.cache", "r"),
 		Dir("/etc/ld.so.conf.d", "r"),
+	})
+
+	stdio = load([]*Path{
+		File("/dev/full", "rw"),
+		File("/dev/zero", "r"),
+		File("/dev/fd", "r"),
+		File("/dev/stdin", "rw"),
+		File("/dev/stdout", "rw"),
+		File("/dev/urandom", "r"),
+		Dir("/dev/log", "w"),
+		Dir("/usr/share/locale", "r"),
+		File("/proc/self/cmdline", "r"),
+		File("/usr/share/zoneinfo", "r"),
+		File("/usr/share/common-licenses", "r"),
+		File("/proc/sys/kernel/ngroups_max", "r"),
+		File("/proc/sys/kernel/cap_last_cap", "r"),
+		File("/proc/sys/vm/overcommit_memory", "r"),
+	})
+
+	tty = load([]*Path{
+		File("/dev/tty", "rw"),
+		File("/dev/console", "rw"),
+		File("/etc/terminfo", "r"),
+		Dir("/usr/lib/terminfo", "r"),
+		Dir("/usr/share/terminfo", "r"),
+	})
+
+	tmp = load([]*Path{
+		Dir("/tmp", "rwc"),
+	})
+
+	vminfo = load([]*Path{
+		File("/proc/stat", "r"),
+		File("/proc/meminfo", "r"),
+		File("/proc/cpuinfo", "r"),
+		File("/proc/diskstats", "r"),
+		File("/proc/self/maps", "r"),
+		File("/proc/sys/kernel/version", "r"),
+		File("/sys/devices/system/cpu", "r"),
+	})
+
+	dns = load([]*Path{
+		File("/etc/hosts", "r"),
+		File("/hostname", "r"),
+		File("/etc/services", "r"),
+		File("/etc/protocols", "r"),
+		File("/etc/resolv.conf", "r"),
+	})
+}
+
+func load(paths []*Path) []*Path {
+	result := make([]*Path, 0, len(paths))
+	for _, p := range paths {
+		if _, err := os.Stat(p.path); err == nil {
+			result = append(result, p)
+		}
 	}
+	return result
 }
 
 // Equal returns true if p is equal to o in terms
@@ -83,7 +145,7 @@ func (p *Path) Equal(o *Path) bool {
 	}
 }
 
-// Hash simply returns the path element of p.
+// Hash returns the path element of p.
 func (p *Path) Hash() string {
 	return p.path
 }
@@ -110,8 +172,47 @@ func Dir(path, mode string) *Path {
 //
 // Use Shared when allowing the execution of dynamically linked binaries.
 func Shared() *Path {
-	return &Path{path: "", mode: "s"}
+	return &Path{mode: modeShared}
 }
+
+// Stdio creates a Path representing the common files and directories
+// needed for standard I/O operations.
+func Stdio() *Path {
+	return &Path{mode: modeStdio}
+}
+
+// TTY creates a path representing common files needed for terminal
+// operations.
+func TTY() *Path {
+	return &Path{mode: modeTTY}
+}
+
+// Tmp creates a Path representing the common files and directories
+// needed for reading and writing to the system tmp space.
+func Tmp() *Path {
+	return &Path{mode: modeTmp}
+}
+
+// VMInfo creates a Path representing the common files and directories
+// needed for virtual machines and system introspection.
+func VMInfo() *Path {
+	return &Path{mode: modeVMInfo}
+}
+
+// DNS creates a Path representing the common files needed for DNS
+// related operations.
+func DNS() *Path {
+	return &Path{mode: modeDNS}
+}
+
+const (
+	modeShared = "1"
+	modeStdio  = "2"
+	modeTTY    = "3"
+	modeTmp    = "4"
+	modeVMInfo = "5"
+	modeDNS    = "6"
+)
 
 func newPath(path, mode string, dir bool) *Path {
 	if !IsProperPath(path) {
